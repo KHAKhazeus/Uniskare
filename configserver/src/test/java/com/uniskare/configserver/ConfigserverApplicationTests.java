80412aaf90fcbf1fdaab3ae6f9ac89748ff4ae30
KHAKhazeus <khazeus@outlook.com>
2019-12-06 21:59:11 +0800

    config server online

---
+++ configserver/src/test/java/com/uniskare/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniskare.configserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
