54cf7473d5d1689a26d6c38457f466e2d03bd171
LkFiveeeeeeee <290115116@qq.com>
2019-12-10 23:46:11 +0800

    skill_eureka

---
+++ src/test/java/com/uniskare/eureka_skill/EurekaSkillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniskare.eureka_skill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSkillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
