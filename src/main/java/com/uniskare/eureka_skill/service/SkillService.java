ede9b910ef1321637497d9987bf0fefa49a05af5
mooooon333 <742932097@qq.com>
2019-12-15 11:55:37 +0800

    refactor, add service

--- src/main/java/com/uniskare/eureka_skill/service/SkillService.java
+++ src/main/java/com/uniskare/eureka_skill/service/SkillService.java
@@ -1,7 +1,10 @@
 package com.uniskare.eureka_skill.service;
 
 import com.alibaba.fastjson.JSONArray;
+import com.uniskare.eureka_skill.entity.Skill;
+
+import java.util.List;
 
 public interface SkillService {
-    public JSONArray getAllOrders();
+    public List<Skill> findAll();
 }
