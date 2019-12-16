4d98d443db38eea34f6fe80fdf8c5964ce4c210b
hangshuaibi <1440620378@qq.com>
2019-12-16 21:03:26 +0800

    [ADD & FIX] Add page logic & test order apis

---
+++ src/main/java/com/uniskare/eureka_skill/service/Helper/BackendException.java
@@ -0,0 +1,12 @@
+package com.uniskare.eureka_skill.service.Helper;
+
+/**
+ * @author : SCH001
+ * @description :
+ */
+public class BackendException extends Exception {
+    public BackendException(String msg)
+    {
+        super(msg);
+    }
+}
