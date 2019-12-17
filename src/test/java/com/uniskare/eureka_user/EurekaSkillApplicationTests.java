756aab32c99f8067d55b7ddc74f00943acf40f0a
LkFiveeeeeeee <290115116@qq.com>
2019-12-17 20:38:48 +0800

    fix test bugs

--- src/test/java/com/uniskare/eureka_user/EurekaSkillApplicationTests.java
+++ src/test/java/com/uniskare/eureka_user/EurekaSkillApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaSkillApplicationTests {
+public class EurekaSkillApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
