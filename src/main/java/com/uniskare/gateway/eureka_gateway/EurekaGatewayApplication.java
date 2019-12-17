70f84aa02f49c5128c9e8fc47fcfb376ede96036
LkFiveeeeeeee <290115116@qq.com>
2019-12-17 17:23:01 +0800

    gateway config

---
+++ src/main/java/com/uniskare/gateway/eureka_gateway/EurekaGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.uniskare.gateway.eureka_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniskare/gateway/eureka_gateway/EurekaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniskare.gateway.eureka_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
