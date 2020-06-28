9b3ec4f6fb24ecc63c8cb70e7257ed39f9abfeee
LkFiveeeeeeee <290115116@qq.com>
2020-06-29 00:31:45 +0800

    IT userController v3

--- src/main/java/com/uniskare/eureka_user/controller/UserController.java
+++ src/main/java/com/uniskare/eureka_user/controller/UserController.java
@@ -2,9 +2,12 @@ package com.uniskare.eureka_user.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 import com.uniskare.eureka_user.controller.Response.BaseResponse;
 import com.uniskare.eureka_user.controller.Response.Code;
+import com.uniskare.eureka_user.controller.Response.ResponseMessage;
 import com.uniskare.eureka_user.dto.CodeResponse;
+import com.uniskare.eureka_user.repository.MessageRepo;
 import com.uniskare.eureka_user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,21 +46,27 @@ public class UserController {
     @RequestMapping(value = "/getLoginCode",method = RequestMethod.POST)
     public BaseResponse verityLoginCode(@RequestParam("code") String code) throws IOException {
         CodeResponse codeResponse = null;
+        BaseResponse baseResponse = new BaseResponse((new Timestamp(System.currentTimeMillis())).toString()
+                , Code.OK
+                , Code.NO_ERROR_MESSAGE
+                , "loc"
+                , "/user/register"
+                , false);
         if(!code.equals("")){
             String params = "appid=" + wxAppid +"&secret="+wxSecret+"&js_code="+code+"&grant_type=authorization_code";
             String url = "https://api.weixin.qq.com/sns/jscode2session?"+params;
             String response = restTemplate.getForObject(url,String.class);
+
+
             logger.info("response:" + response);
             codeResponse = mapper.readValue(response,CodeResponse.class);
-            userService.register(codeResponse.getOpenid());
+            if(codeResponse.getSession_key() != null){
+                userService.register(codeResponse.getOpenid());
+            }
+
         }
 
-        return new BaseResponse((new Timestamp(System.currentTimeMillis())).toString()
-                , Code.OK
-                , Code.NO_ERROR_MESSAGE
-                , Code.NO_MESSAGE_AVAILABLE
-                , "/user/register"
-                , codeResponse);
+        return baseResponse;
     }
 
     @RequestMapping(value = "/login",method = RequestMethod.POST)
